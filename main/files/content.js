b8fd79f353b7b1a489377739871b25571cb3cd96
devtracer <devoft1@gmail.com>
2024-11-03 22:36:39 +0330

    Delete main/files/content.js

--- main/files/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "setLoadTime") {
-    const formattedTime = (request.loadTime / 1000);
-    chrome.action.setBadgeText({ text: formattedTime });
-  }
-});
