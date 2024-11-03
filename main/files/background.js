f0e468b653431bd1b69b18956c7f901e566c0fc0
devtracer <devoft1@gmail.com>
2024-11-03 23:33:43 +0330

    Update background.js

--- main/files/background.js
+++ main/files/background.js
@@ -1,3 +1,4 @@
+// Displaying the page load time on the extension's icon badge
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "setLoadTime") {
     const formattedTime = (request.loadTime / 1000).toFixed(2);
