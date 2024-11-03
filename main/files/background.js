0c3464d94cb9c0813fc13720f33b584fbe17004d
devtracer <devoft1@gmail.com>
2024-11-03 21:59:14 +0330

    Update background.js

--- main/files/background.js
+++ main/files/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "setLoadTime") {
-    const formattedTime = (request.loadTime / 1000).toFixed(2);
+    const formattedTime = (request.loadTime / 1000);
     chrome.action.setBadgeText({ text: formattedTime });
   }
 });
